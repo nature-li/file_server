ca82f4f1b050ef4ff3ca32860dd7397fb4012eec
liyanguo <lyg@meitu.com>
2018-04-17 21:10:43 +0800

    change some icon

--- template/js/pages/public.js
+++ template/js/pages/public.js
@@ -45,14 +45,14 @@ $.showConfirm = function (str, func_ok, func_close) {
 
 // 解锁侧边栏
 function unlock_pin() {
-    $("#lock-side-bar").removeClass("glyphicon-triangle-bottom");
-    $("#lock-side-bar").addClass("glyphicon-triangle-left");
+    $("#lock-side-bar").removeClass("glyphicon-lock");
+    $("#lock-side-bar").addClass("glyphicon-pushpin");
 }
 
 // 锁住侧边栏
 function lock_pin() {
-    $("#lock-side-bar").removeClass("glyphicon-triangle-left");
-    $("#lock-side-bar").addClass("glyphicon-triangle-bottom");
+    $("#lock-side-bar").removeClass("glyphicon-pushpin");
+    $("#lock-side-bar").addClass("glyphicon-lock");
 }
 
 // 隐藏侧边栏
@@ -60,22 +60,21 @@ $(".side-bar-hidden").click(function (e) {
     e.preventDefault();
     $("#wrapper").removeClass("toggled");
     $("#expand-side-bar").removeClass("hidden-self");
-    unlock_pin();
 });
 
 // 隐藏侧边栏
 $(".side-bar-condition-hidden").click(function (e) {
-    if ($("#lock-side-bar").hasClass("glyphicon-triangle-left")) {
+    if ($("#lock-side-bar").hasClass("glyphicon-pushpin")) {
         e.preventDefault();
         $("#wrapper").removeClass("toggled");
         $("#expand-side-bar").removeClass("hidden-self");
-        unlock_pin();
     }
 });
 
 // 展示侧边栏
 $(".side-bar-show").click(function (e) {
     e.preventDefault();
+    unlock_pin();
     $("#wrapper").addClass("toggled");
     $("#expand-side-bar").addClass("hidden-self");
 });
@@ -83,7 +82,7 @@ $(".side-bar-show").click(function (e) {
 // 加/解锁侧边栏
 $("#lock-side-bar").click(function (e) {
     e.preventDefault();
-    if ($("#lock-side-bar").hasClass("glyphicon-triangle-left")) {
+    if ($("#lock-side-bar").hasClass("glyphicon-pushpin")) {
         lock_pin();
     } else {
         unlock_pin();
@@ -93,7 +92,7 @@ $("#lock-side-bar").click(function (e) {
 // 加/解锁侧边栏
 $(document).click(function (event) {
     event.preventDefault();
-    if ($("#lock-side-bar").hasClass("glyphicon-triangle-left")) {
+    if ($("#lock-side-bar").hasClass("glyphicon-pushpin")) {
         if (!$(event.target).closest("#sidebar-wrapper, #expand-side-bar").length) {
             if ($("#wrapper").hasClass("toggled")) {
                 $("#wrapper").removeClass("toggled");
