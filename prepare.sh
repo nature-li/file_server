0576587296919e1d985a57d83a741deb2f9e025f
liyanguo <lyg@meitu.com>
2018-04-23 16:46:48 +0800

    add prepare sh

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+export GOPATH=$(pwd $(dirname $0))
+
+mkdir -p $GOPATH/src/golang.org/x && cd $GOPATH/src/golang.org/x && git clone https://github.com/golang/image.git
+
+go get github.com/afocus/captcha
+go get github.com/mattn/go-sqlite3
+go get gopkg.in/yaml.v2
