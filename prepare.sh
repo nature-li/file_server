b02fa9a630b7294130df392522613e6764396dba
liyanguo <lyg@meitu.com>
2018-04-23 16:48:55 +0800

    pwd

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ export GOPATH=$(pwd $(dirname $0))
 
 mkdir -p $GOPATH/src/golang.org/x && cd $GOPATH/src/golang.org/x && git clone https://github.com/golang/image.git
 
+cd ${GOPATH}
 go get github.com/afocus/captcha
 go get github.com/mattn/go-sqlite3
 go get gopkg.in/yaml.v2
